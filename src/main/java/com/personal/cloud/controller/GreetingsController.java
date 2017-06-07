0878c89cfea86d2413d83323e3af1e78e315abda
Jose Garcia <josgar@unimallt161.unibet.com>
2017-06-07 17:26:27 +0100

    Greetings service

--- src/main/java/com/personal/cloud/controller/GreetingsController.java
+++ src/main/java/com/personal/cloud/controller/GreetingsController.java
@@ -2,10 +2,12 @@ package com.personal.cloud.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by josgar on 02/06/2017.
@@ -15,8 +17,14 @@ import java.util.Map;
 public class GreetingsController {
 
     @GetMapping(path = "/greetings/{name}")
-    Map<String, String> greetings(@PathVariable String name){
-        return Collections.singletonMap("Hello !!! ", name);
+    Map<String, String> greetings(@PathVariable String name,
+                                  @RequestHeader("x-forwarded-host") Optional<String> host,
+                                  @RequestHeader("x-forwarded-port") Optional<Integer> port) {
+
+        host.ifPresent(System.out::println);
+        port.ifPresent(System.out::println);
+
+        return Collections.singletonMap("message", "Helloooo " + name);
     }
 
 }
