e8d0b2b0f3a9f0f0df2e7d26561bb9e203cc6b4b
Jose Garcia <josgar@unimallt161.unibet.com>
2017-06-07 17:26:27 +0100

    Greetings service

--- src/main/java/com/personal/cloud/controller/GreetingsController.java
+++ src/main/java/com/personal/cloud/controller/GreetingsController.java
@@ -5,10 +5,12 @@ import org.springframework.boot.actuate.endpoint.SystemPublicMetrics;
 import org.springframework.boot.actuate.metrics.dropwizard.DropwizardMetricServices;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by josgar on 02/06/2017.
@@ -25,7 +27,12 @@ public class GreetingsController {
     }
 
     @GetMapping(path = "/greetings/{name}")
-    Map<String, String> greetings(@PathVariable String name){
+    Map<String, String> greetings(@PathVariable String name,
+                                  @RequestHeader("x-forwarded-host") Optional<String> host,
+                                  @RequestHeader("x-forwarded-port") Optional<Integer> port) {
+
+        host.ifPresent(System.out::println);
+        port.ifPresent(System.out::println);
 
         metricServices.increment("counter.greetings");
 
@@ -37,5 +44,4 @@ public class GreetingsController {
 
         return greeting;
     }
-
 }
